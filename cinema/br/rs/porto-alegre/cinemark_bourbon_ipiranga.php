78c837c03c322cc89775a38ebd869b7c31a3ce10
eduardosasso <eduardo.sasso@gmail.com>
2011-01-20 17:26:07 -0200

    Suporte para adaptador Cinemark

--- cinema/br/rs/porto-alegre/cinemark_bourbon_ipiranga.php
+++ cinema/br/rs/porto-alegre/cinemark_bourbon_ipiranga.php
@@ -1,7 +1,7 @@
 <?php
 include realpath($_SERVER["DOCUMENT_ROOT"]) . '/classes.php';
 
-class cinemark_bourbon_ipiranga extends GoogleMoviesAdapter {
+class cinemark_bourbon_ipiranga extends CinemarkAdapter {
 
 	public function get_cinema() {
 		$cinema = new Cinema();
@@ -18,9 +18,12 @@ class cinemark_bourbon_ipiranga extends GoogleMoviesAdapter {
 		return $cinema;			
 	}
 
-	public function get_url() {
-		return "http://google.com.br/movies?near=Alvorada+RS&tid=8ae8a7a6a67628ba";
+	protected function get_id_cidade_do_site() {
+		return "11";
 	}
-
+	protected function get_id_cinema_do_site() {
+		return "695";
+	}
+	
 }
 ?>
---
+++ test/adapters/CinemarkAdapterTest.php
@@ -0,0 +1,17 @@
+<?php
+include realpath($_SERVER["DOCUMENT_ROOT"]) . '/classes.php';
+
+//phpunit test/adapters/CinemarkAdapterTest.php
+class CinemarkAdapterTest extends PHPUnit_Framework_TestCase {
+	
+	function test_all(){
+		$cinemark = new cinemark_bourbon_ipiranga();
+		$cinema = $cinemark->scrape();
+		
+		echo "<pre>";
+		print_r($cinema);
+		echo "</pre>";		
+	}
+	
+}
+?>
